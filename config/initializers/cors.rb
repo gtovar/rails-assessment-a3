407e3977d3d84c24cd1b0e140f57fbe452fc47f0
Gilberto Tovar <tovarninogilberto@gmail.com>
2026-02-03 16:06:42 -0600

    fix(cors): allow app.invoices.gilbertotovar.com

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:8080', 'https://zip-plan-builder.vercel.app'
+    origins 'http://localhost:8080', 'https://zip-plan-builder.vercel.app', 'https://app.invoices.gilbertotovar.com'
 
     resource '*',
       headers: :any,
